183064027e2ca516e225ad50470a67a899e4262a
Miao Wang <miaowang@google.com>
2017-06-09 11:22:31 -0700

    Add O API to slang_version, and auto convert it to 24.
      - No new runtime APIs added in O. We should keep the bitcode version
      to be 24.
    
    Bug: 63141957
    Test: mm and manually invoked llvm-rs-cc with -target-api 26 and
          verified that it is converted to 24.
    
    Change-Id: Ie9366718393a6a76e6557ac9f05f9877670b1b9d
    (cherry picked from commit 38d179cec358bd910586d6672cf2ebea6d8283c2)

--- rs_cc_options.cpp
+++ rs_cc_options.cpp
@@ -290,9 +290,10 @@ bool ParseArguments(const llvm::ArrayRef<const char *> &ArgsIn,
 
   if (Opts.mTargetAPI == 0) {
     Opts.mTargetAPI = UINT_MAX;
-  } else if (Opts.mTargetAPI == SLANG_N_MR1_TARGET_API) {
+  } else if (Opts.mTargetAPI == SLANG_N_MR1_TARGET_API ||
+             Opts.mTargetAPI == SLANG_O_TARGET_API) {
     // Bug: http://b/35767071
-    // No new APIs for N_MR1, convert to N.
+    // No new APIs for N_MR1 or O, convert to N.
     Opts.mTargetAPI = SLANG_N_TARGET_API;
   }
 
